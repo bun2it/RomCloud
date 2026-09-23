be7bf535d3dd10519ba86fc081d477601f4acdf4
bun2it <bun2it@gmail.com>
2026-09-23 09:30:23 +0700

    v1.3.1: Bump version

--- src/ota/UpdateManager.h
+++ src/ota/UpdateManager.h
@@ -7,7 +7,7 @@
 
 namespace RomCloud {
 
-constexpr const char* APP_VERSION = "1.3.0";
+constexpr const char* APP_VERSION = "1.3.1";
 constexpr const char* GITHUB_REPO = "bun2it/RomCloud";
 constexpr const char* VERSION_MANIFEST_URL = "https://raw.githubusercontent.com/bun2it/RomCloud/main/version.json";
 
